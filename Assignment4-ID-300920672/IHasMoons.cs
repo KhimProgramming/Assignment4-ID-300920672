93bd5f324a2835218e2b416d0118c1c71de8060f
Khim Tang <ktang33@my.centennialcollege.ca>
2017-07-13 21:35:09 -0400

    Added IHasMoons Interface

---
+++ Assignment4-ID-300920672/IHasMoons.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Khim Tang
+ * Date: July 13, 2017
+ * Description: This Interface lists the signature for any GiantPlanet Subclass
+ * Version: 0.1 - declared the moon properties
+ */
+
+namespace Assignment4_ID_300920672
+{
+    public interface IHasMoons
+    {
+        // Public properties
+        int Moons { get; set; }
+    }
+}
---
+++ Assignment4-ID-300920672/IHasRings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment4_ID_300920672
+{
+    public interface IHasRings
+    {
+    }
+}
